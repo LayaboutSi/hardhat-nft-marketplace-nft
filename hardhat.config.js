2c97906b09fabdd7fb981709fa42099dadc1bb35
Simon Young <simon_young_au@yahoo.com.au>
2022-09-26 10:37:36 +1000

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,67 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+//require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL;
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+      //      blockConfirmations: 1,
+    },
+    localhost: {
+      chainId: 31337,
+      gas: 6000000,
+    },
+    kovan: {
+      url: KOVAN_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 42,
+      blockConfirmations: 6,
+      gas: 6000000,
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+      blockConfirmations: 6,
+      gas: 6000000,
+    },
+  },
+  solidity: { compilers: [{ version: "0.8.9" }, { version: "0.6.6" }, { version: "0.6.12" }, { version: "0.4.19" }] },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: { default: 1 },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH", // npmjs.com/package/hardhat-gas-reporter   /// ETH, BNB, MATIC, AVAX, HT, MOVR
+  },
+  mocha: {
+    timeout: 100000000,
+  },
+};
---
+++ helper-hardhat-config.js
@@ -0,0 +1,50 @@
+const { ethers } = require("hardhat");
+
+const WETH_TOKEN_ADDRESS = process.env.WETH_TOKEN_ADDRESS || "";
+
+const networkConfig = {
+  31337: {
+    name: "localhost",
+    wethToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    lendingPoolAddressesProvider: "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+    ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+    daiEthPriceFeed: "0x773616E4d11A78F511299002da57A0a94577F1f4",
+    daiToken: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    entranceFee: ethers.utils.parseEther("0.01"),
+    mintFee: "10000000000000000", // 0.01 ETH
+    callbackGasLimit: "500000",
+    gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+    subscriptionId: "0",
+    interval: "30",
+  },
+  // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+  // Default one is ETH/USD contract on Kovan
+  42: {
+    name: "kovan",
+    ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+    gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+  },
+  4: {
+    name: "rinkeby",
+    keepersUpdateInterval: "30",
+    vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+    ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+    entranceFee: "100000000000000000", // 0.1 ETH
+    gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+    subscriptionId: "20799",
+    callbackGasLimit: "500000",
+    interval: "30",
+    mintFee: "10000000000000000", // 0/01 ETH
+  },
+};
+
+const DECIMALS = "18";
+const INITIAL_PRICE = "200000000000000000000";
+const developmentChains = ["hardhat", "localhost"];
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+  DECIMALS,
+  INITIAL_PRICE,
+};
